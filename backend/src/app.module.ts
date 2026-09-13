1a47cae8b792997c55145f90400cd9f26e3519f2
ziad-kamel <ziad.mohamed.kamell.184@gmail.com>
2026-09-14 01:59:16 +0300

    init NestJs

---
+++ backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { createObserveModule } from '@nestjs/observe';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+
+export const { ObserveModule, ObserveInstrument } = createObserveModule();
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
