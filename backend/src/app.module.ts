5984b2e8711345e1274c5002dc81ed05ff4b5999
ziad-kamel <ziad.mohamed.kamell.184@gmail.com>
2026-09-14 03:34:19 +0300

    configure mongoDB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './mongoose-config/mongoose-config.service.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}), // to configure the .env vars across the app
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService //seting up the env vars with cleaner way
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MongooseConfigService],
 })
 export class AppModule {}
